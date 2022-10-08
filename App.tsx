7f4d3f1ab9e5398f764bbf9ab6a77006760e21f9
dtthong31 <hongblack2000@gmail.com>
2022-10-09 04:15:57 +0700

    add lib

--- App.tsx
+++ App.tsx
@@ -1,15 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-Amplify.configure(awsconfig);
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
 
-export default function App() {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+Amplify.configure(awsconfig)
+
+import { withAuthenticator } from 'aws-amplify-react-native';
+import '@aws-amplify/ui-react/styles.css';
+
+function App() {
+  // const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+
+  // if (!isLoadingComplete) {
+  //   return null;
+  // } else {
+  return (
+    <SafeAreaProvider>
+      <Navigation colorScheme={colorScheme} />
+      <StatusBar />
+    </SafeAreaProvider>
+  );
+  // }
 }
+
+export default withAuthenticator(App)
