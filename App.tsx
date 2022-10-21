882863f256ce3a683504c462ce8201057d75d589
dtthong31 <hongblack2000@gmail.com>
2022-10-21 11:27:55 +0700

    app just UI not configure

--- App.tsx
+++ App.tsx
@@ -6,16 +6,16 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-import { Amplify } from 'aws-amplify'
-import awsconfig from './src/aws-exports'
-Amplify.configure(awsconfig)
+// import { Amplify } from 'aws-amplify'
+// import awsconfig from './src/aws-exports'
+// Amplify.configure(awsconfig)
 
-import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react-native';
-import { Auth, API, graphqlOperation } from 'aws-amplify';
+// import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react-native';
+// import { Auth, API, graphqlOperation } from 'aws-amplify';
 
 //  Graphql
-import { getTodo } from './queries';
-import { createTodo } from './mutations';
+// import { getTodo } from './queries';
+// import { createTodo } from './mutations';
 
 const randomImage = [
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwCZE-3NGtzDSPHzbwo_9FyPvfkCwAVWbW6Q&usqp=CAU",
@@ -28,52 +28,52 @@ const randomImage = [
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-  const getRandomImages = () => {
-    return randomImage[Math.floor(Math.random() * randomImage.length)];
-  }
-  useEffect(() => {
-    const fecthUser = async () => {
-      // get Authenticated user from auth
-      const userInfo = await Auth.currentAuthenticatedUser({ bypassCachee: true });
+  // const getRandomImages = () => {
+  //   return randomImage[Math.floor(Math.random() * randomImage.length)];
+  // }
+  // useEffect(() => {
+  //   const fecthUser = async () => {
+  //     // get Authenticated user from auth
+  //     const userInfo = await Auth.currentAuthenticatedUser({ bypassCachee: true });
 
-      // get the user from Backendwith the user id from auth
-      if (userInfo) {
-        const userData = await API.graphql(
-          graphqlOperation(
-            getTodo,
-            {
-              id: userInfo.attributes.sub,
-            }
-          )
-        )
-        if (userData.data.getTodo) {
-          console.log("user is already registered in database");
-          return;
+  //     // get the user from Backendwith the user id from auth
+  //     if (userInfo) {
+  //       const userData = await API.graphql(
+  //         graphqlOperation(
+  //           getTodo,
+  //           {
+  //             id: userInfo.attributes.sub,
+  //           }
+  //         )
+  //       )
+  //       if (userData.data.getTodo) {
+  //         console.log("user is already registered in database");
+  //         return;
 
-        }
-        console.log(userData);
+  //       }
+  //       console.log(userData);
 
-        const newUser = {
-          id: userInfo.attributes.sub,
-          name: userInfo.username,
-          imageUri: getRandomImages(),
-          status: "Hey, I'm available.",
-        }
+  //       const newUser = {
+  //         id: userInfo.attributes.sub,
+  //         name: userInfo.username,
+  //         imageUri: getRandomImages(),
+  //         status: "Hey, I'm available.",
+  //       }
 
-        await API.graphql(
-          graphqlOperation(
-            createTodo,
-            {
-              input: newUser
-            }
-          )
-        )
-      }
-      //  if there is no user in db with the id, then create one 
-    }
+  //       await API.graphql(
+  //         graphqlOperation(
+  //           createTodo,
+  //           {
+  //             input: newUser
+  //           }
+  //         )
+  //       )
+  //     }
+  //     //  if there is no user in db with the id, then create one 
+  //   }
 
-    fecthUser();
-  })
+  //   fecthUser();
+  // })
 
 
   if (!isLoadingComplete) {
@@ -87,46 +87,47 @@ function App() {
     );
   }
 }
-const signUpConfig = {
-  header: 'My customized Sign up',
-  hideAllDefaults: true,
-  signUpFields: [
-    {
-      lable: "Full name",
-      key: "name",
-      require: true,
-      displayOver: 1,
-      type: "string",
-    },
-    {
-      lable: "Email",
-      key: "email",
-      require: true,
-      displayOver: 2,
-      type: "string",
-    },
-    {
-      lable: "Username",
-      key: "preferred_username",
-      require: true,
-      displayOver: 3,
-      type: "string",
-    },
-    {
-      lable: "Password",
-      key: "password",
-      require: true,
-      displayOver: 4,
-      type: "string",
-    }
-  ]
-}
-const themesCustom = {
-  ...AmplifyTheme,
-  button: {
-    ...AmplifyTheme.button,
-    backgroundColor: 'blue',
-    borderRadius: 10,
-  }
-}
-export default withAuthenticator(App, { signUpConfig, theme: themesCustom });
+// const signUpConfig = {
+//   header: 'My customized Sign up',
+//   hideAllDefaults: true,
+//   signUpFields: [
+//     {
+//       lable: "Full name",
+//       key: "name",
+//       require: true,
+//       displayOver: 1,
+//       type: "string",
+//     },
+//     {
+//       lable: "Email",
+//       key: "email",
+//       require: true,
+//       displayOver: 2,
+//       type: "string",
+//     },
+//     {
+//       lable: "Username",
+//       key: "preferred_username",
+//       require: true,
+//       displayOver: 3,
+//       type: "string",
+//     },
+//     {
+//       lable: "Password",
+//       key: "password",
+//       require: true,
+//       displayOver: 4,
+//       type: "string",
+//     }
+//   ]
+// }
+// const themesCustom = {
+//   ...AmplifyTheme,
+//   button: {
+//     ...AmplifyTheme.button,
+//     backgroundColor: 'blue',
+//     borderRadius: 10,
+//   }
+// }
+// export default withAuthenticator(App, { signUpConfig, theme: themesCustom });
+export default App;
